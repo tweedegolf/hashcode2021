7f9f0620d2f6a5f7ab4cbd242199b1e50532dd75
David Venhoek <david@tweedegolf.com>
2021-02-25 19:15:15 +0100

    Data model.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+struct Street {
+    b: isize,
+    e: isize,
+    name: String,
+    l: isize,
+}
 
+struct Car {
+    route: Vec<isize>
+}
+
+struct Problem {
+    d: isize,
+    i: isize,
+    f: isize,
+    streets: Vec<Street>,
+    cars: Vec<Car>,
+}
+
+struct LightPeriod {
+    street: isize,
+    period: isize,
+}
+
+struct Solution {
+    schedules: Vec<Vec<LightPeriod>>,
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
